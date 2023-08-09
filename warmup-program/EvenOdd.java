c51362ceebbdf30637b66d1afbfbc260c63e0d56
prra0622 <prateek.rao@netcracker.com>
2023-08-10 02:03:01 +0530

    Added EvenOdd Program

---
+++ warmup-program/EvenOdd.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class EvenOdd{
+    public static void main(String[] args) {
+        int num;
+
+        System.out.println("Enter any Integer number: ");
+
+        Scanner scanner = new Scanner(System.in);
+        num = scanner.nextInt();
+
+        if(num % 2 ==0){
+            System.out.println("The number is even");
+        }
+        else{
+            System.out.println("The number is odd");
+        }
+        
+    }
+}
