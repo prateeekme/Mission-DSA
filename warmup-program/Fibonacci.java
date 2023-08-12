c99c34a91474614023c50fd0006f06cefeac074d
prra0622 <prateek.rao@netcracker.com>
2023-08-12 16:13:08 +0530

    Added Fabonacci Program

--- warmup-program/Fibonacci.java
+++ warmup-program/Fibonacci.java
@@ -3,24 +3,26 @@ import java.util.Scanner;
 public class Fibonacci {
 
     public static void main(String[] args) {
-        
         int inputInteger = 0;
-        int result = 0;
 
         System.out.println("Please enter the Fibonacci number count: ");
-
-        Scanner scanner = new Scanner(System.in);
-        inputInteger = scanner.nextInt();
-
-        if(inputInteger == 0){
-            result = 0;
+        try (Scanner scanner = new Scanner(System.in)) {
+            inputInteger = scanner.nextInt();
         }
-        else{
-            result += result;
+        
+        for (int i = 0; i < inputInteger; i++) {
+            int fibonacciNumber = getFibonacci(i);
+            System.out.print(fibonacciNumber + " ");
         }
-
-        System.out.println(result);
-
     }
-    
+
+    private static int getFibonacci(int n) {
+        if (n <= 0) {
+            return 0;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return getFibonacci(n - 1) + getFibonacci(n - 2);
+        }
+    }
 }
