1e28907a5ae1a3040b830e5432905b6aed46a2b4
prra0622 <prateek.rao@netcracker.com>
2024-09-28 20:36:59 +0530

    Arrays - Stack - MinStack

--- Codopedia/minimum_stack.java
+++ Codopedia/minimum_stack.java
@@ -1,5 +1,35 @@
 package Codopedia;
 
+/*
+ * Minimum Stack
+ *
+ * Design a stack class that supports the following operations:
+ * 1. push(val): Push the element 'val' onto the stack.
+ * 2. pop(): Remove the element on the top of the stack.
+ * 3. top(): Retrieve the top element of the stack.
+ * 4. getMin(): Retrieve the minimum element in the stack.
+ *
+ * The goal is to ensure each operation runs in O(1) time.
+ *
+ * Example 1:
+ * Input: ["MinStack", "push", 1, "push", 2, "push", 0, "getMin", "pop", "top", "getMin"]
+ * Output: [null,null,null,null,0,null,2,1]
+ *
+ * Explanation:
+ * MinStack minStack = new MinStack();
+ * minStack.push(1);
+ * minStack.push(2);
+ * minStack.push(0);
+ * minStack.getMin();  // returns 0
+ * minStack.pop();
+ * minStack.top();     // returns 2
+ * minStack.getMin();  // returns 1
+ *
+ * Constraints:
+ * - The values will always be within the range: -2^31 <= val <= 2^31 - 1.
+ * - The stack will never be empty when pop, top, or getMin operations are called.
+ */
+
 import java.util.Stack;
 
 public class minimum_stack {
