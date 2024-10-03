8b4c47ecfe65235d1fe7ae3b33720adbbe2e2213
prra0622 <prateek.rao@netcracker.com>
2024-10-03 12:54:56 +0530

    Sorting - Binary Range - First Bad Version

---
+++ Codopedia/first_bad_version.java
@@ -0,0 +1,62 @@
+package Codopedia;
+
+/*
+You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
+
+Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
+
+You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
+
+
+
+Example 1:
+
+Input: n = 5, bad = 4
+Output: 4
+Explanation:
+call isBadVersion(3) -> false
+call isBadVersion(5) -> true
+call isBadVersion(4) -> true
+Then 4 is the first bad version.
+Example 2:
+
+Input: n = 1, bad = 1
+Output: 1
+
+
+Constraints:
+
+1 <= bad <= n <= 231 - 1
+ */
+
+public class first_bad_version {
+
+    /* The isBadVersion API is defined in the parent class VersionControl.
+      boolean isBadVersion(int version); */
+
+        public int firstBadVersion(int n) {
+
+            int left = 1;
+            int right = n;
+
+            int ans = 0;
+            while (left <= right){
+                int mid = left + (right - left) / 2;
+
+                if(isBadVersion(mid)){
+                    ans = mid;
+                    right = mid - 1;
+                }else{
+                    left = mid + 1;
+                }
+            }
+
+            return ans;
+        }
+
+        //Ignore iBAdVersion
+    private boolean isBadVersion(int mid) {
+            return true;
+    }
+}
+
